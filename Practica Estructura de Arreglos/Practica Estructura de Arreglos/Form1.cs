9a19c7aa8e7b87d875c13589a542f237683862c3
BryanHernandez <36205875+BryanHernandez@users.noreply.github.com>
2018-04-19 18:32:21 -0700

    Add files via upload

---
+++ Practica Estructura de Arreglos/Practica Estructura de Arreglos/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Estructura_de_Arreglos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Inventario inventario = new Inventario();
+        Auto autoN;
+
+        private void button1_Click(object sender, EventArgs e)//Agregar boton
+        {
+            autoN = new Auto();
+            txtPosicion.Value = inventario.cantidad + 2;
+            if (inventario.cantidad < 15)
+            {
+                leer();
+                inventario.Agregar(autoN);
+                Borrar();
+            }
+            else
+            {
+                MessageBox.Show("LLeno", "Espacio", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void Borrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void read()//nota cambia el leer por read cambio raro como apunte pregunta el profesor sobre este error
+        {
+            throw new NotImplementedException();
+        }
+    
+        private void button4_Click(object sender, EventArgs e)//Insertar
+        {
+            autoN = new Auto();
+            inventario.posicion = Convert.ToInt16(txtPosicion.Value) - 1;
+            leer();
+            inventario.Insertar(autoN);
+            Borrar();
+        }
+
+        private void leer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            inventario.posicion = Convert.ToInt16(txtPosicion.Value) - 1;
+            inventario.Eliminar();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string placa = txtPlacas.Text;
+            inventario.Buscar(placa);
+            txtList.Text = inventario.ToString();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            inventario.Lista();
+            txtList.Text = inventario.ToString();
+            txtPosicion.Value = inventario.cantidad + 1;
+        }
+    }
+}
---
+++ Practica Estructura de Arreglos/Practica Estructura de Arreglos/Inventario.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Practica_Estructura_de_Arreglos
+{
+    class Inventario
+    {
+        int _Cantidad;
+        int _Posicion;
+        string Datos = "";
+        public int posicion { get { return _Posicion; } set { _Posicion = value; } }
+
+        Auto[] lisAuto = new Auto[15];
+        public int cantidad { get { return _Cantidad; } }
+        public Inventario()
+
+        {
+            _Cantidad = 0;
+            _Posicion = 0;
+        }
+
+        public void Agregar(Auto Car)
+        {
+            if (_Cantidad < 14)
+
+            {
+                lisAuto[_Cantidad] = Car;
+                _Cantidad++;
+            }
+            else
+            {
+                lisAuto[_Cantidad] = Car;
+            }
+        }
+        private void Ordenar()//Nota este codigo cortarlo para entregar practica es perteneciente a la practica 2
+        { }
+     public void Insertar(Auto Car)
+
+     {
+         Mover();
+         lisAuto[_Posicion] = Car;
+
+         if (_Cantidad < 14)
+
+         {
+             _Cantidad++;
+         }
+     }
+     
+        private void Mover()
+        {
+            for (int a = _Cantidad; a > _Posicion; a--)
+            {
+                lisAuto[a] = lisAuto[a - 1];
+            }
+        }
+        public void Eliminar()
+
+        {
+            if (lisAuto[_Posicion] != null)
+            {
+                lisAuto[_Posicion] = null;
+                MoverBorrar();
+                _Cantidad--;
+            }
+
+        }
+        private void MoverBorrar()
+        {
+            for (int a = _Posicion; a < _Cantidad; a++)
+
+            {
+                lisAuto[a] = lisAuto[a + 1];
+            }
+        }
+        public void Lista()
+        {
+            Datos = "";
+            for (int a = 0; a <= (_Cantidad - 1); a++)
+            {
+                Datos += (a + 1) + "._\t" + lisAuto[a].ToString() + Environment.NewLine;
+            }
+        }
+        public void Buscar(string placa)
+        {
+            bool existe = false;
+            Datos = "";
+            int a = 0;
+            while (existe == false && a <= _Cantidad)
+            {
+                if (lisAuto[a].placa == placa)
+                {
+                    Datos = (a + 1) + "._\t" + lisAuto[a].ToString();
+                    MessageBox.Show("Carro encontrado", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                    a = _Cantidad;
+                    existe = true;
+                }
+                a++;
+            }
+        }
+        public override string ToString()
+        {
+            return Datos;
+        }
+    }
+}
